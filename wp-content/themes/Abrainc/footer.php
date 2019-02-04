a1dc86253bbd7220f5d7b8bc559420ae381a4194
alexandre-yokota <alexandre.yokota@tboom.net>
2019-02-04 19:03:51 -0200

    alteração / tag egoi

--- wp-content/themes/Abrainc/footer.php
+++ wp-content/themes/Abrainc/footer.php
@@ -152,6 +152,23 @@
 
 <script src="<?=ABRAINC_URL?>/wp-content/themes/Abrainc/scripts/scripts.js"></script>
 
+<script type="text/javascript">
+var _egoiaq = _egoiaq || [];
+(function(){
+                var u=(("https:" == document.location.protocol) ? "https://egoimmerce.e-goi.com/" : "http://egoimmerce.e-goi.com/");
+                _egoiaq.push(['setClientId', "315639"]);
+                _egoiaq.push(['setTrackerUrl', u+'collect']);
+                _egoiaq.push(['trackPageView']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.type='text/javascript';
+                g.defer=true;
+                g.async=true;
+                g.src=u+'egoimmerce.js';
+                s.parentNode.insertBefore(g,s);
+})();
+</script>
+
+
 <?php wp_footer(); ?>
 </body>
 </html>
